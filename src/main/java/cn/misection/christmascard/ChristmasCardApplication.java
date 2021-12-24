cf0b409d9dc846917697940f40ac166f8eb0097d
zhengjiefu <zhengjiefu@kuaishou.com>
2021-12-24 16:05:31 +0800

    Initial commit

---
+++ src/main/java/cn/misection/christmascard/ChristmasCardApplication.java
@@ -0,0 +1,13 @@
+package cn.misection.christmascard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChristmasCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/misection/christmascard/ChristmasCardApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.misection.christmascard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristmasCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
